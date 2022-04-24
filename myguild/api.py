3a118542abc86f365a977af246e7739fde023ce4
Garrett Smith <garrett@placeon.earth>
2022-04-24 12:56:14 -0500

    User specific API keys

--- myguild/api.py
+++ myguild/api.py
@@ -8,19 +8,25 @@ DiscourseClientError = pydiscourse.exceptions.DiscourseClientError
 
 
 def init():
+    api_key = _my_guild_env("MY_GUILD_API_KEY")
+    api_username = _my_guild_env("MY_GUILD_API_USER")
+    client = pydiscourse.DiscourseClient(
+        "https://my.guild.ai",
+        api_username=api_username,
+        api_key=api_key,
+    )
+    _patch_client(client)
+    return client
+
+
+def _my_guild_env(name):
     try:
-        api_key = os.environ["MY_GUILD_API_KEY"]
+        return os.environ[name]
     except KeyError:
         raise SystemExit(
-            "MY_GUILD_API_KEY environment must be set for this command\n"
-            "Try 'source <( gpg -d api-creds.gpg )' to set it."
+            f"{name} environment must be set for this command\n"
+            "Try 'source <( gpg -d api-<user>.gpg )' to set it."
         )
-    else:
-        client = pydiscourse.DiscourseClient(
-            "https://my.guild.ai", api_username="guildai", api_key=api_key
-        )
-        _patch_client(client)
-        return client
 
 
 def _patch_client(client):
@@ -28,9 +34,11 @@ def _patch_client(client):
     # default). Replace api._request to ensure it's None, which works
     # fine.
     request0 = client._request
+
     def patched_request(*args, **kw):
         json = kw.pop("json", None)
         return request0(*args, json=json, **kw)
+
     client._request = patched_request
 
 
